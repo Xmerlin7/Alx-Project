bcbb5d356d33d3cf810ce32b41915678232cb031
Seif Allah <saifeldarageely@gmail.com>
2024-12-15 01:18:36 +0200

    Add POST route to Express server and improve response formatting

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const app = express()
+const res = require("express/lib/response");
+const app = express();
 
-app.get("/", (req, res)=> {
-  res.send("!شكرا على المساعدة يا عرص")
-})
-app.get("/home", (req, res)=> {
-    res.send(" يا عرص")
-  })
+app.get("/", (req, res) => {
+  res.send("!شكرا على المساعدة يا عرص");
+});
+app.get("/home", (req, res) => {
+  res.send(" يا عرص");
+});
+app.post("/post", (req, res) => {
+  res.send("hell");
+});
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log("Server running on port 3000");
 });
