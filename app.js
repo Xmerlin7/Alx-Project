7a2f25277d5266a3d675c235bbad4b7b1ddde8b0
Seif Allah <saifeldarageely@gmail.com>
2024-12-15 01:49:04 +0200

    Refactor POST route to accept JSON body and query parameters for improved response

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
-
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("!شكرا على المساعدة يا عرص");
 });
 app.get("/home", (req, res) => {
   res.send(" يا عرص");
 });
-app.post("/post/:n1/:n2", (req, res) => {
-  res.send(`the numbers are ${req.params.n1} and ${req.params.n2}`);
+app.post("/post", (req, res) => {
+  res.send(`hello ${req.body.name} your age is ${req.query.age}`);
 });
 app.listen(3000, () => {
   console.log("Server running on port 3000");
