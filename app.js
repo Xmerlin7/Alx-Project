6a28fa7afc09b67c2e8b61b7fe6eb194145898e8
Seif Allah <saifeldarageely@gmail.com>
2024-12-15 01:14:15 +0200

    Add home route to Express server with a simple response

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express()
 app.get("/", (req, res)=> {
   res.send("!شكرا على المساعدة يا عرص")
 })
+app.get("/home", (req, res)=> {
+    res.send(" يا عرص")
+  })
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
