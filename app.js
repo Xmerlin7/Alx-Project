cf84bdcbec0bd6314b17c76617a97aafec7e97b6
Seif Allah <saifeldarageely@gmail.com>
2024-12-17 16:42:27 +0200

    Connect to MongoDB and log connection status

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
+const moongo = require("mongoose");
+moongo
+  .connect(
+    "mongodb+srv://seifahmedjr7:WhGZw792pHX8xAHP@cluster0.6trnl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("loged successfully to DB");
+  })
+  .catch((e) => {
+    console.log("couldn't connect tho the DB", e);
+  });
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Welcome Home, Mate")
+  res.send("Welcome Home, Mate");
 });
 app.get("/home", (req, res) => {
   res.send(" يا عرص");
