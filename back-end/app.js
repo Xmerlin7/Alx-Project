bf988f7eeca29a4944b771c93d8fe73234dd8d72
Seif Allah <saifeldarageely@gmail.com>
2024-12-19 19:22:07 +0200

    Initialize Express server and set up basic route

---
+++ back-end/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import moongoose from "mongoose";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Book sotre with MERN stack");
+});
+app.listen(PORT, () => {
+  console.log(`App is listening on port: ${PORT}`);
+});
