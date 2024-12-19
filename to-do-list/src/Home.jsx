defa1c49258671792fb2fee5e7bbc14943cd9b4b
Seif Allah <saifeldarageely@gmail.com>
2024-12-19 16:33:31 +0200

    Add state management for todos in Home component and display message for empty list

--- to-do-list/src/Home.jsx
+++ to-do-list/src/Home.jsx
@@ -1,12 +1,17 @@
-// import React from "react";
-
+import { useState } from "react";
 import Create from "./Create";
 
 const Home = () => {
+  const [todos, setTodos] = useState([]);
   return (
     <div>
       <h2>Todo List</h2>
-      <Create/>
+      <Create />
+      {todos.length === 0 ? (
+        <div>No Record</div>
+      ) : (
+        todos.map((todo) => <div key="todo">{todo}</div>)
+      )}
     </div>
   );
 };
