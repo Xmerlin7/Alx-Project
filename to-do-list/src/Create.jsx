c63f6a226e9bda8c45a816d8107aeb2ea8ba14b5
Seif Allah <saifeldarageely@gmail.com>
2024-12-19 16:40:28 +0200

    Add styling for Home and Create components; update structure for better layout

--- to-do-list/src/Create.jsx
+++ to-do-list/src/Create.jsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-
+import "./App.css";
 const Create = () => {
   return (
-    <div>
-      <input type="text" name="" id="" />
-      <button type="button">Add </button>
+    <div className="createForm">
+      <input className="input" type="text" name="" id="" />
+      <button className="button" type="button">Add </button>
     </div>
   );
 };
