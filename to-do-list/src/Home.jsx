cf35777f9e6b97ef011145b88dc7dc104fc32355
Seif Allah <saifeldarageely@gmail.com>
2024-12-19 15:45:12 +0200

    Refactor Home component for cleaner syntax and remove commented import

--- to-do-list/src/Home.jsx
+++ to-do-list/src/Home.jsx
@@ -1,10 +1,6 @@
-import React from "react";
+// import React from "react";
 
-const Home =  () => {
-    return(
-        <div>
-            Home
-        </div>
-    )
-}
-export default Home
+const Home = () => {
+  return <div>Home</div>;
+};
+export default Home;
