37297ceee1c0c9fce165980dc224d3e291c65dc1
Seif Allah <saifeldarageely@gmail.com>
2024-12-22 08:31:18 +0200

    Fix typo in binarySearch function and log array length

--- back-end/test.js
+++ back-end/test.js
@@ -2,5 +2,6 @@ const arr = [1, 2, 3, 4, 5, 43, 5, 34, 534, 5, 3435, 34, 5, 34];
 console.log(binarySerach(arr, 9));
 
 function binarySerach (arr, num) {
-    
+    const len = arr.length;
+    console.log((len))
 }
