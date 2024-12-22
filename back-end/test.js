978d8d7b2ae0439fe6ba345839043e4a12e771c9
Seif Allah <saifeldarageely@gmail.com>
2024-12-22 08:43:45 +0200

    Refactor binarySearch function to improve logic and update test cases

--- back-end/test.js
+++ back-end/test.js
@@ -1,7 +1,23 @@
-const arr = [1, 2, 3, 4, 5, 43, 5, 34, 534, 5, 3435, 34, 5, 34];
-console.log(binarySerach(arr, 9));
+const arr = [1, 2, 3, 4, 5, 43];
+console.log(binarySerach(arr, 1));
 
-function binarySerach (arr, num) {
-    const len = arr.length;
-    console.log((len))
+function binarySerach(arr, num) {
+  const len = arr.length;
+  let start = 0;
+  let end = len;
+  let mid = Math.floor((start + end) / 2)
+  while (start <= end) {
+    if (arr[mid] === num) {
+      return mid;
+    }
+    if (arr[mid] > num) {
+      end = mid;
+      mid = Math.floor((start + end) / 2)
+    }
+    if (arr[mid] < num) {
+      start = mid;
+      mid = Math.floor((start + end) / 2)
+    }
+  }
+  return -1;
 }
