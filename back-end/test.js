3a625f2ceb338aa43708dc11f527fe21643ca588
Seif Allah <saifeldarageely@gmail.com>
2024-12-22 08:47:30 +0200

    Fix binarySearch function logic and update test case

--- back-end/test.js
+++ back-end/test.js
@@ -1,22 +1,19 @@
 const arr = [1, 2, 3, 4, 5, 43];
-console.log(binarySerach(arr, 1));
+console.log(binarySerach(arr, 43));
 
 function binarySerach(arr, num) {
   const len = arr.length;
   let start = 0;
-  let end = len;
-  let mid = Math.floor((start + end) / 2)
+  let end = len - 1;
   while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
     if (arr[mid] === num) {
-      return mid;
+      return mid + 1;
     }
     if (arr[mid] > num) {
-      end = mid;
-      mid = Math.floor((start + end) / 2)
-    }
-    if (arr[mid] < num) {
-      start = mid;
-      mid = Math.floor((start + end) / 2)
+      end = mid - 1;
+    } else {
+      start = mid + 1;
     }
   }
   return -1;
