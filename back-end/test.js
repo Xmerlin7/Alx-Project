b96ed5ff17d27856447d32ef0377de008c966485
Seif Allah <saifeldarageely@gmail.com>
2024-12-24 01:57:24 +0200

    Implement findSmallest function to identify the smallest number in unsortedArray

--- back-end/test.js
+++ back-end/test.js
@@ -1,2 +1,15 @@
-const unsortedArray = [34, 7, 23, 32, 5, 62, 78, 1, 45, 19];
-console.log(unsortedArray);
+const unsortedArray = [34, 7, 23, 32, 5, 62, -9, -1, 45, 19];
+
+const smallest = findSmallest(unsortedArray);
+console.log(unsortedArray[smallest]);
+function findSmallest(arr) {
+  let smallest = arr[0];
+  let smallestIndex = 0;
+  for (let i = 1; i <= arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+      smallestIndex = i;
+    }
+  }
+  return smallestIndex;
+}
