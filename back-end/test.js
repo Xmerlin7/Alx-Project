e3ba8249b5a0eae72260d71571679fa0e418c3a3
Seif Allah <saifeldarageely@gmail.com>
2024-12-24 02:16:05 +0200

    Add selectionSort function to sort an array using the findSmallest helper

--- back-end/test.js
+++ back-end/test.js
@@ -2,10 +2,12 @@ const unsortedArray = [34, 7, 23, 32, 5, 62, -9, -1, 45, 19];
 
 const smallest = findSmallest(unsortedArray);
 console.log(unsortedArray[smallest]);
+
 function findSmallest(arr) {
   let smallest = arr[0];
   let smallestIndex = 0;
-  for (let i = 1; i <= arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
+    // Changed loop condition
     if (arr[i] < smallest) {
       smallest = arr[i];
       smallestIndex = i;
@@ -13,3 +15,16 @@ function findSmallest(arr) {
   }
   return smallestIndex;
 }
+
+function selectionSort(arr) {
+  let newArr = [];
+  const originalLength = arr.length; // Avoid dynamic length changes
+  for (let i = 0; i < originalLength; i++) {
+    // Corrected loop
+    let smallest = findSmallest(arr);
+    newArr.push(arr.splice(smallest, 1)[0]); // Extract the value from the returned array
+  }
+  return newArr;
+}
+
+console.log(selectionSort(unsortedArray));
