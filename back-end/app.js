3ece189ca612627a52de1cfb150d62258e48a452
Seif Allah <saifeldarageely@gmail.com>
2025-01-07 16:58:45 +0200

    Refactor book routes into a separate module and improve error handling

--- back-end/app.js
+++ back-end/app.js
@@ -1,117 +1,31 @@
-import express from "express";
-import { PORT, mongoDBURL } from "./config.js";
-import mongoose from "mongoose"; // Fix the typo here
-import { Book } from "./models/booksModel.js";
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
+
+// Middleware for parsing request body
 app.use(express.json());
-// Set strictQuery to false to handle the deprecation warning
-mongoose.set("strictQuery", false);
+app.use(cors());
 
-// Define a route for the root URL
-app.get("/", (req, res) => {
-  console.log(req);
-  return res.status(234).send("Book store with MERN");
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
-// Route for saving a new book
-app.post("/books", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res.status(400).send({
-        message: "Send all required fields: title, author, publishYear",
-      });
-    }
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      publishYear: req.body.publishYear,
-    };
+app.use('/books', booksRoute);
 
-    const book = await Book.create(newBook);
-
-    return res.status(201).send(book);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send({ message: error.message });
-  }
-});
-app.get("/books", async (req, res) => {
-  try {
-    const books = await Book.find({});
-    return res.status(200).json({
-      count: books.length,
-      data: books,
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send({ message: error.message });
-  }
-});
-
-app.get("/books/:id", async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const book = await Book.findById(id);
-
-    if (!book) {
-      return res.status(404).json({ message: "Book not found" });
-    }
-
-    return res.status(200).json({ message: "data retrievd", book: book });
-  } catch (error) {
-    console.error(error.message);
-
-    if (error.name === "CastError") {
-      return res.status(400).json({ message: "Invalid book ID" });
-    }
-
-    res.status(500).json({ message: "Server error" });
-  }
-});
-app.put("/books/:id", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res.status(400).send({
-        message: "Send all required fields: title, author, publishYear",
-      });
-    }
-    const { id } = req.params;
-    const result = await Book.findByIdAndUpdate(id, req.body);
-    if (!result) {
-      return res.status(404).json({ message: "book not found" });
-    }
-    return res.status(200).send({ message: "Book updated successfully" });
-  } catch (error) {
-    console.log(error.message);
-    return res.status(500).send({ message: error.message });
-  }
-});
-app.delete("/books/:id", async (req, res) => {
-  try {
-
-    const { id } = req.params;
-    const result = await Book.findByIdAndDelete(id);
-    if (!result) {
-      return res.status(404).json({ message: "book not found" });
-    }
-    return res.status(200).send({ message: "Book updated successfully" });
-  } catch (error) {
-    console.log(error.message);
-    return res.status(500).send({ message: error.message });
-  }
-});
-// Connect to the MongoDB database and start the server
 mongoose
   .connect(mongoDBURL)
   .then(() => {
-    console.log("App connected to DB");
+    console.log('App connected to database');
     app.listen(PORT, () => {
-      console.log(`App is listening on port: ${PORT}`);
+      console.log(`App is listening to port: ${PORT}`);
     });
   })
   .catch((error) => {
-    console.error("Error connecting to the database:", error);
-    process.exit(1); // Exit the process if the connection fails
+    console.log(error);
   });
