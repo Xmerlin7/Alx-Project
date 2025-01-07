3d470fef84a1958ce09a06c42feb9ee2591689a2
Seif Allah <saifeldarageely@gmail.com>
2025-01-07 19:06:55 +0200

    Remove unused ESLint disable comment for prop-types in BookModal component

--- front-end/src/components/home/BookModal.jsx
+++ front-end/src/components/home/BookModal.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { AiOutlineClose } from 'react-icons/ai';
 import { PiBookOpenTextLight } from 'react-icons/pi';
 import { BiUserCircle } from 'react-icons/bi';
