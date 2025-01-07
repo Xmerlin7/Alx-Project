937f83c0579b0913b56b965b02d58d9d1b577df9
Seif Allah <saifeldarageely@gmail.com>
2025-01-07 14:19:14 +0200

    Add response handling for GET route to retrieve all books

--- back-end/app.js
+++ back-end/app.js
@@ -39,6 +39,7 @@ app.post("/books", async (req, res) => {
 app.get("/books", async (req, res) => {
   try {
     const books = await Book.find({});
+    return res.status(200).send(books)
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
